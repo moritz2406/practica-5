747d0e9abe5db1a14d69f3b9e8d8e583316e8620
moritz2406 <122538931+moritz2406@users.noreply.github.com>
2023-02-16 14:12:10 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let control2 = 0
 basic.forever(function () {
-	
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+    basic.pause(100)
+    while (control2 > 0) {
+        control2 += 1
+        pins.analogWritePin(AnalogPin.P0, -1)
+        basic.pause(1000)
+    }
+    while (control2 > 0) {
+        control2 += 1
+        pins.analogWritePin(AnalogPin.P0, -1)
+        basic.pause(1000)
+    }
 })
