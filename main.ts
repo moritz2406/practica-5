f461116fe58a2c95a85635a2b023bd22e505ad7d
moritz2406 <122538931+moritz2406@users.noreply.github.com>
2023-02-16 14:19:25 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 let control2 = 0
+led.enable(false)
+control2 += 1
 basic.forever(function () {
     for (let index = 0; index < 4; index++) {
         pins.digitalWritePin(DigitalPin.P0, 1)
@@ -8,12 +10,12 @@ basic.forever(function () {
     basic.pause(100)
     while (control2 > 0) {
         control2 += 1
-        pins.analogWritePin(AnalogPin.P0, -1)
+        pins.analogWritePin(AnalogPin.P0, control2)
         basic.pause(1000)
     }
     while (control2 > 0) {
         control2 += 1
-        pins.analogWritePin(AnalogPin.P0, -1)
+        pins.analogWritePin(AnalogPin.P0, control2)
         basic.pause(1000)
     }
 })
